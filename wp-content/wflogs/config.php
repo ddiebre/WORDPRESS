f509eff284fbc2da2e844f6231dc6ee6d044bddb
ddiebre@gmail.com <ddiebre@gmail.com>
2021-05-01 07:43:46 +0000

    version000

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1620392656;s:7:"authKey";s:64:"3rZ1<T$(TK$I2+%*S>7N,FK|cKU!(NaF54o5ez:=EzCIsf90!|$jo6h5Cf9o(#PM";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3392;}
